e7bb82651beb05424bd5ee1fd711233210c5a362
jyeonnb <49296434+jyeonnb@users.noreply.github.com>
2020-06-25 17:02:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Jiyeon!!');
+    res.send('I like Jiyeon!');
 });
 
 app.get('/start', function(req, res){
